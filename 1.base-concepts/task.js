ed9628c8ddb55ebec7721aa6467f5d494242035f
elena-aether <e.v.noack@gmail.com>
2022-02-04 13:05:13 -0700

    task1 code addad

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,26 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr = [x1, x2];
+    console.log('roots:' +arr)
+
+  } else if (d === 0) {
+    let x1 = -b / (2 * a);
+    arr = [x1];
+    console.log('roots:' +arr)
+
+  } else if (d < 0) {
+   arr = []; 
+   console.log('roots:' +arr)
+
+  } else {
+  console.log('incorrect input')
+};
   return arr; // array
 }
 
