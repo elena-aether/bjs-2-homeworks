0a9b49cfc594078fa6589492d89e9d652fc907c5
elena-aether <e.v.noack@gmail.com>
2022-02-07 22:29:57 -0700

    tasks 1 & 2 completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
-function compareArrays(arr1, arr2) {
-  let result;
+  function compareArrays(arr1, arr2) {
+    let result = arr1.every((index) => arr1.length === arr2.length && arr1[index] === arr2[index]);
+    return result; // boolean
+   };
 
-  // Ваш код
-
-  return result; // boolean
-}
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr
+  .filter((item) => item > 0 && item % 3 === 0)
+  .map((item) => item * 10);
   return resultArr; // array
 }
